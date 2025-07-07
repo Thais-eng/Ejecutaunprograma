7cc845a8c9508a9f5bf8eef060722bfba6dff14f
tatasiguano@puce.edu.ec <tatasiguano@puce.edu.ec>
2025-07-06 19:45:20 -0500

    Primer commit o actualización

--- Scripts/run_phylogeny.sh
+++ Scripts/run_phylogeny.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-# Script para alinear secuencias y construir árbol filogenético Canidae
-
 module load mafft
 module load iqtree/2.2.6
 
 echo "Alineando secuencias con MAFFT..."
-mafft --auto ../Data/canidae_cytb_subset.fasta > aligned_cytb.fasta
+mafft --auto ./data/canidos_cytb.fasta > aligned_cytb.fasta
 
 echo "Construyendo árbol con IQ-TREE..."
-iqtree -s aligned_cytb.fasta -m MFP -nt AUTO -bb 1000 -alrt 1000
+iqtree2 -s aligned_cytb.fasta -m MFP -nt AUTO -bb 1000 -alrt 1000
 
-echo "Moviendo resultados a la carpeta Results..."
-mv *.treefile ../Results/canidae_tree.nwk
-mv *.log ../Results/
-mv *.iqtree ../Results/
-mv *.ckp.gz ../Results/
-mv *.bionj ../Results/
-mv *.mldist ../Results/
-mv *.model.gz ../Results/
-mv *.contree ../Results/
-mv *.splits.nex ../Results/
-mv *.alrt* ../Results/
-mv *.png ../Results/ 2>/dev/null  # por si existe una imagen del árbol
+echo "Limpiando y moviendo resultados..."
+mkdir -p Results
+rm -rf Results/*
+
+mv *.treefile ./Results/canidae_tree.nwk
+mv *.log ./Results/
+mv *.iqtree ./Results/
+mv *.ckp.gz ./Results/
+mv *.bionj ./Results/
+mv *.mldist ./Results/
+mv *.model.gz ./Results/
+mv *.contree ./Results/
+mv *.splits.nex ./Results/
+mv *.alrt* ./Results/
+mv *.png ./Results/ 2>/dev/null
 
 echo "Árbol construido y guardado en Results/"
 
